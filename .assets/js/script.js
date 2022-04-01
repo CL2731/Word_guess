5d462e7b7972ac6b644fadd285f048cdd7cc4b77
Caitlin Lindauer <clilos018@gmail.com>
2022-04-01 15:31:36 -0600

    noted layout of game

---
+++ .assets/js/script.js
@@ -0,0 +1,20 @@
+// create a button 
+// have an event listener listen for click of button                    (11, 12)
+// after click we will start game - function
+// display hidden word                                                  (19, 20 of class)
+// add another event listener that listens for keydown
+// if keydown = any correct letter, then show letter                    (15, 16 referance)
+// add timer when game starts                                           (09, 10 from class for help)
+// user wins when all letters are displayed before time runs out
+// user loses if all letters are not displayed before time runs out
+//  us localstorage to keep track of wins & loses                       (20, 21 referance)
+
+// make button eventlistener ("click", func that starts game and timer)
+// Within function (game){
+//      timer function
+//      eventlistener ? (keydown) function - check if letter shows
+//      if ( checks timer === all shown) Win
+//      if (! timer === all shown ) lose
+//      local storage wins
+//      local storage losses
+//      }
